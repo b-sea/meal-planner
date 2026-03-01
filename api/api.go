0b34e6dda49a7c04cb14aac4f696d9cd6bd2a319
b-sea <20363821+b-sea@users.noreply.github.com>
2026-02-28 22:36:43 -0500

    setup cmd, api, repository layers (#3)

---
+++ api/api.go
@@ -0,0 +1,96 @@
+// Package api implements the meal prepping API.
+package api
+
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/b-sea/go-server/server"
+	"github.com/b-sea/meal-planner/dash"
+	"github.com/b-sea/meal-planner/mock"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+)
+
+const defaultLogLevel = zerolog.InfoLevel
+
+// Service is the main API service.
+type Service struct {
+	dash dash.DASH
+	http *server.Server
+}
+
+// New creates a new API service with the default server.
+func New(version string, config Config) *Service {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack //nolint: reassign
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	level, err := zerolog.ParseLevel(config.Logger.Level)
+	if err != nil {
+		level = defaultLogLevel
+	}
+
+	log := zerolog.New(
+		zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: time.RFC3339,
+		},
+	).Level(level).With().Timestamp().Logger()
+
+	zerolog.DefaultContextLogger = &log
+
+	return NewService(
+		server.New(
+			context.Background(),
+			&mock.Recorder{},
+			server.WithVersion(version),
+			server.WithPort(config.HTTP.Port),
+			server.WithReadTimeout(time.Duration(config.HTTP.ReadTimeout)*time.Second),
+			server.WithReadTimeout(time.Duration(config.HTTP.WriteTimeout)*time.Second),
+		),
+	)
+}
+
+// NewService creates a new API service with a custom server.
+func NewService(svr *server.Server) *Service {
+	service := &Service{
+		dash: dash.New(),
+		http: svr,
+	}
+	service.handle()
+
+	return service
+}
+
+// Start the API service.
+func (s *Service) Start() error {
+	ctx := context.Background()
+
+	if err := s.http.Start(ctx); err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msg("start server")
+
+		return err //nolint: wrapcheck
+	}
+
+	return nil
+}
+
+// Stop the API service.
+func (s *Service) Stop() error {
+	ctx := context.Background()
+
+	if err := s.http.Stop(ctx); err != nil {
+		zerolog.Ctx(ctx).Error().Err(err).Msg("stop server")
+
+		return err //nolint: wrapcheck
+	}
+
+	zerolog.Ctx(ctx).Info().Msg("stop server")
+
+	return nil
+}
+
+func (s *Service) handle() {
+	s.http.Router().PathPrefix("/api").Subrouter()
+}
---
+++ mock/food.go
@@ -0,0 +1,44 @@
+package mock
+
+import (
+	"github.com/b-sea/meal-planner/food"
+	"github.com/google/uuid"
+)
+
+var _ food.Repository = (*Foodstuff)(nil)
+
+type Foodstuff struct {
+	FindFoodResult     []*food.Food
+	FindFoodErr        error
+	GetFoodByIDResult  *food.Food
+	GetFoodByIDErr     error
+	GetFoodByIDsResult []*food.Food
+	GetFoodByIDsErr    error
+	CreateFoodErr      error
+	UpdateFoodErr      error
+	DeleteFoodErr      error
+}
+
+func (m *Foodstuff) FindFood() ([]*food.Food, error) {
+	return m.FindFoodResult, m.FindFoodErr
+}
+
+func (m *Foodstuff) GetFoodByID(id uuid.UUID) (*food.Food, error) {
+	return m.GetFoodByIDResult, m.GetFoodByIDErr
+}
+
+func (m *Foodstuff) GetFoodByIDs(ids []uuid.UUID) ([]*food.Food, error) {
+	return m.GetFoodByIDsResult, m.GetFoodByIDsErr
+}
+
+func (m *Foodstuff) CreateFood(food *food.Food) error {
+	return m.CreateFoodErr
+}
+
+func (m *Foodstuff) UpdateFood(food *food.Food) error {
+	return m.UpdateFoodErr
+}
+
+func (m *Foodstuff) DeleteFood(id uuid.UUID) error {
+	return m.DeleteFoodErr
+}
